3e755dbd11a817913f0f774611d7e4e41e7e20d6
kodok-ijho <dyudhiantoro@gmail.com>
2025-06-08 19:30:56 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
+const express = require('express');
 const puppeteer = require('puppeteer');
+const app = express();
+const port = process.env.PORT || 3000;
 
-(async () => {
+app.get('/', async (req, res) => {
   const browser = await puppeteer.launch({
     args: ['--no-sandbox', '--disable-setuid-sandbox']
   });
   const page = await browser.newPage();
   await page.goto('https://example.com');
-  console.log(await page.title());
+  const title = await page.title();
   await browser.close();
-})();
+  res.send(`Title is: ${title}`);
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
