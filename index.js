ba50eb0e12dba8cd3914ea43e105f201a724279d
kodok-ijho <dyudhiantoro@gmail.com>
2025-06-08 19:18:53 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch({
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
+  });
+  const page = await browser.newPage();
+  await page.goto('https://example.com');
+  const title = await page.title();
+  console.log('Page title:', title);
+  await browser.close();
+})();
